c544b3cb5a98b61331cd4e0f25097ecda170b1c1
SyaifudinRamadhan <syaifudinramadhan@gmail.com>
2022-11-17 00:41:25 +0700

    setup postgres

--- config/database.js
+++ config/database.js
@@ -6,30 +6,30 @@
 /** Destruct environment variable to get database configuration */
 const {
   DB_USERNAME = "postgres",
-  DB_PASSWORD = "Syaifudin_1012",
-  DB_HOST = "127.0.0.1",
-  DB_NAME = "binar_ch_6_",
+  DB_PASSWORD = "5IPJRgQ1hEEx3g0DGoM8",
+  DB_HOST = "containers-us-west-104.railway.app",
+  DB_NAME = "railway",
 } = process.env;
 
 module.exports = {
   development: {
     username: DB_USERNAME,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_development`,
+    database: `${DB_NAME}`,
     host: DB_HOST,
     dialect: "postgres",
   },
   test: {
     username: DB_USERNAME,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_development`,
+    database: `${DB_NAME}`,
     host: DB_HOST,
     dialect: "postgres",
   },
   production: {
     username: DB_USERNAME,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_development`,
+    database: `${DB_NAME}`,
     host: DB_HOST,
     dialect: "postgres",
   },
